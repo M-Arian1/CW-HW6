bb101e21c65cd6ada77e91eb085b5c5cac42309f
arian <arian.iravaninia@gmail.com>
2022-12-01 17:23:41 +0330

    commit 8

--- project1.c
+++ project1.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
-    float a = 0, b = 0;
-    scanf("%f %f", &a, &b);
-    printf("%lld", a * b);
-=======
     int a = 0, b = 0;
->>>>>>> parent of a08f38d (commit2)
+    scanf("%d %d", &a, &b);
+    printf("%d", a * b);
 }
