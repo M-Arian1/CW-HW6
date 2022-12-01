f2359349e57d75ad485cd1cbf4fb0dc1ad6f5398
arian <arian.iravaninia@gmail.com>
2022-12-01 17:25:01 +0330

    commit 9

--- project1.c
+++ project1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int a = 0, b = 0;
-    scanf("%d %d", &a, &b);
-    printf("%d", a * b);
+    int a = 0, b = 0, c = 0;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d", a * b * c);
 }
