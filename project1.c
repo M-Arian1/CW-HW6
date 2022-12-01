1db79def868bb705112170b5bce31e2029691909
arian <arian.iravaninia@gmail.com>
2022-12-01 16:12:30 +0330

    commit4 : overflow solved2!

--- project1.c
+++ project1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    long int a = 0, b = 0;
-    scanf("%ld %ld", &a, &b);
-    printf("%ld", a * b);
+    long long int a = 0, b = 0;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a * b);
 }
