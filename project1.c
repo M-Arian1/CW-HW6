83eec2bd140e3b0765f61dc164af7bfba262c962
arian <arian.iravaninia@gmail.com>
2022-12-01 17:20:31 +0330

    Revert "commit2"
    This reverts commit a08f38d16490b7e79a4ad7ea9633c7da2a1ff2bd.

--- project1.c
+++ project1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
+<<<<<<< HEAD
     float a = 0, b = 0;
     scanf("%f %f", &a, &b);
     printf("%lld", a * b);
+=======
+    int a = 0, b = 0;
+>>>>>>> parent of a08f38d (commit2)
 }
